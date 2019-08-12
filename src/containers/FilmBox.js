bec761c6a84a4824e87c1babc47bd2cc0fb30897
Steve Gossip <stephengossip@gmail.com>
2019-08-12 18:41:06 +0100

    created containers and components folders and a FilmBox file. Printing HelloWorld to the browser.

---
+++ src/containers/FilmBox.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class FilmBox extends Component {
+  render() {
+    return(
+      <div className="film-box">
+        Hello World! I'm a FilmBox.
+      </div>
+    )
+
+  }
+}
